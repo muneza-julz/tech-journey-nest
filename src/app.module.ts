33790e40bfcc898d01b3d62a50f49be046e5ae64
Muneza Jules Eusèbe <munezajulz@gmail.com>
2026-09-12 12:27:49 +0200

    Removed old modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { ItemsModule } from './items/items.module';
+
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true }),
-        DatabaseModule,
-        ItemsModule],
+        DatabaseModule],
     controllers: [],
     providers: [],
 })
